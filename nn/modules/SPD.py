a1e8d3576bc596f26f687c888667244da0fc6539
niurouli <134353796+YongliLiu@users.noreply.github.com>
2024-01-18 17:33:41 +0800

    Update SPD.py

--- nn/modules/SPD.py
+++ nn/modules/SPD.py
@@ -4,22 +4,6 @@ import torch.nn as nn
 
 
 class space_to_depth(nn.Module):
-    # Changing the dimension of the Tensor
-    def __init__(self, dimension=1):
-        super().__init__()
-        self.d = dimension
-
-    def forward(self, x):
-         return torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1)
 
 
-class SPDConv(nn.Module):
-    # Changing the dimension of the Tensor
-    def __init__(self, c1, c2, dimension=1):
-        super().__init__()
-        self.d = dimension
-        self.cv = nn.Conv2d(4 * c1, c2, 1, 1)
-
-    def forward(self, x):
-        x_out = torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1)
-        return self.cv(x_out)
+# This section will be made public upon acceptance of the paper.
